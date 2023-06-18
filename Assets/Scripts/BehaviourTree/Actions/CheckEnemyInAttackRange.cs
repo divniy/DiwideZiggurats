8b18826e3642992182d92ba6b874fc21e47be5c5
Dmitriy Paramonov <divniy@gmail.com>
2023-06-18 05:28:57 +0300

    Refactor CheckEnemyInAttackRange action

--- Assets/Scripts/BehaviourTree/Actions/CheckEnemyInAttackRange.cs
+++ Assets/Scripts/BehaviourTree/Actions/CheckEnemyInAttackRange.cs
@@ -1,31 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 using TheKiwiCoder;
 
-[System.Serializable]
-public class CheckEnemyInAttackRange : ActionNode
+namespace Diwide.Ziggurat.BehaviourTree.Actions
 {
-    public float attackRange;
-    protected override void OnStart() {
-    }
-
-    protected override void OnStop() {
-    }
-
-    protected override State OnUpdate()
+    [Serializable]
+    public class CheckEnemyInAttackRange : ActionNode
     {
-        if (blackboard.target == null) return State.Failure;
-        // float sqrDistanceToTarget = (context.transform.position - blackboard.target.position).sqrMagnitude;
-        // if (sqrDistanceToTarget <= Mathf.Pow(attackRange, 2))
-        // {
-        //     return State.Success;
-        // }
-        if (Vector3.Distance(context.transform.position, blackboard.target.position) <= attackRange)
-        {
-            return State.Success;
+        public float attackRange = 1f;
+        protected override void OnStart() {
         }
 
-        return State.Failure;
+        protected override void OnStop() {
+        }
+
+        protected override State OnUpdate()
+        {
+            if (blackboard.target == null) return State.Failure;
+            var sqrDistanceToTarget = (context.transform.position - blackboard.target.position).sqrMagnitude;
+            return sqrDistanceToTarget <= attackRange * attackRange 
+                ? State.Success : State.Failure;
+        }
     }
 }
